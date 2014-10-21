18ca5060886bf4c9deeb284e65acd2e15a5003c1
Carlos <Carlos@Xapa_Desktop>
2014-10-21 16:29:09 +0200

    Primera beta del gestor de Vehiculos

--- src/main/java/es/vehiculos/CarlosAzanon/GestorVehiculos.java
+++ src/main/java/es/vehiculos/CarlosAzanon/GestorVehiculos.java
@@ -1,5 +1,33 @@
 package es.vehiculos.CarlosAzanon;
 
+import java.util.ArrayList;
+
 public class GestorVehiculos {
 
+    private ArrayList<Vehiculo> vehiculos=new ArrayList<Vehiculo>();
+    private String[] tiposVehiculos = {"Coche A","Coche B","Coche C","Moto","Bicicleta"}; 
+    
+    public boolean addVehiculo(Vehiculo vehiculo){
+        for(Vehiculo v : vehiculos){
+            if(v.getId()==vehiculo.getId()){
+                return false;
+            }
+        }
+        vehiculos.add(vehiculo);
+        return true;
+    }
+    
+    public String[] getTiposVehiculos(){
+        return tiposVehiculos;        
+    }
+    
+    @Override
+    public String toString(){
+        String cadena="";
+        for(Vehiculo v : vehiculos){
+            cadena+=v.toString();
+        }
+        return cadena;
+    }
+    
 }
--- src/main/java/es/vehiculos/CarlosAzanon/MainVehiculos.java
+++ src/main/java/es/vehiculos/CarlosAzanon/MainVehiculos.java
@@ -1,5 +1,28 @@
 package es.vehiculos.CarlosAzanon;
 
-public class MainVehiculos {
+import upm.jbb.IO;
 
+public class MainVehiculos {
+    
+    private GestorVehiculos gestor=new GestorVehiculos();
+    
+    public void añadirVehiculo() {
+        String tipoVehiculo=(String)IO.in.select(gestor.getTiposVehiculos(),"Selecciona tipo de Vehiculo");
+        int id=IO.in.readInt("Identificador del vehiculo");
+        String descripcion=IO.in.readString("Descripcion del vehiculo");
+        gestor.a
+    }
+     
+    public void consultarVehiculos(){
+        IO.out.println(gestor.toString());
+    }
+    
+    public void obtenerAlquiler(){
+        
+    }
+
+    public static void main(String[] args) {
+        IO.in.addController(new MainVehiculos());
+    }
+    
 }
