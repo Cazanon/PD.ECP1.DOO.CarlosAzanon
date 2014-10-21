9c5a2691954948a1fc800e1089325df0cd06eac0
Carlos <Carlos@Xapa_Desktop>
2014-10-21 15:21:41 +0200

    Implementada la clase vehiculo

--- src/main/java/es/vehiculos/CarlosAzanon/Coche.java
+++ src/main/java/es/vehiculos/CarlosAzanon/Coche.java
@@ -2,14 +2,42 @@ package es.vehiculos.CarlosAzanon;
 
 public class Coche extends Vehiculo {
 
-    public Coche(int id, String descripcion) {
+    private final int PORCENTAJE_RANGO_1=100;
+    private final int PORCENTAJE_RANGO_2=80;
+    private final int PORCENTAJE_RANGO_3=50;
+    
+    private final int MAX_DIAS_RANGO_1=3;
+    private final int MAX_DIAS_RANGO_2=7;
+    
+    private Categoria categoria;
+    
+    public Coche(int id, String descripcion, Categoria categoria) {
         super(id, descripcion);
+        this.categoria=categoria;
     }
 
     @Override
+    public double obtenerPrecio(int dias) {        
+        int precio=0;   
+        if(dias<=MAX_DIAS_RANGO_1){
+            precio=dias*(PORCENTAJE_RANGO_1/100)*categoria.getPrecio();
+        }else if(dias>MAX_DIAS_RANGO_1 && dias<=MAX_DIAS_RANGO_2){
+            precio=MAX_DIAS_RANGO_1*(PORCENTAJE_RANGO_1/100)*categoria.getPrecio(); 
+            precio+=(dias-MAX_DIAS_RANGO_1)*(PORCENTAJE_RANGO_2/100)*categoria.getPrecio();
+        }else if(dias>MAX_DIAS_RANGO_2){
+            precio=MAX_DIAS_RANGO_1*(PORCENTAJE_RANGO_1/100)*categoria.getPrecio(); 
+            precio+=(MAX_DIAS_RANGO_2-MAX_DIAS_RANGO_1)*(PORCENTAJE_RANGO_2/100)*categoria.getPrecio();
+            precio+=(dias-MAX_DIAS_RANGO_2)*(PORCENTAJE_RANGO_3/100)*categoria.getPrecio();
+        }else{
+            assert false : "Numero de dias incorrecto";
+        }        
+        return precio;
+    }
+    
+    @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return null;
+        return "Tipo: "+this.getClass().getSimpleName()+"\tId: "+getId()+"\tDescripcion: "+getDescripcion()+"\tCategoria: "+
+                categoria.getClass().getSimpleName()+"\n";
     }
 
 }
